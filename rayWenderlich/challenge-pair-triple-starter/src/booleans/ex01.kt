ba614d73b559bdcc2ae1ad865002e98c0320e512
Thiago Aires <makarrao@gmail.com>
2019-07-31 00:51:42 -0300

    kotlin / flow

---
+++ rayWenderlich/challenge-pair-triple-starter/src/booleans/ex01.kt
@@ -0,0 +1,19 @@
+
+
+fun main(args: Array<String>) {
+  val yes1:Boolean = true
+  val no1:Boolean = false
+
+  val yes2:Boolean = true
+  val no2:Boolean = false
+
+  println("yes1 = yes2 ${yes1 == yes2} no1 = no2 ${no1 == no2}")  
+
+  val doesOneEqualTwo = (1==2)
+
+  println("does one equal two ${doesOneEqualTwo}")
+
+  val myName = "thiago".length < 5
+
+  println("sizename? $myName")
+}
---
+++ rayWenderlich/scope/src/scope.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var hoursWorked = 45.0
+    var price = 0.0
+
+    if (hoursWorked > 40){
+
+        val hoursOver40= hoursWorked - 40
+        price = hoursOver40 * 50
+        hoursWorked -= hoursOver40
+    }
+    price += hoursWorked * 25
+    println(price)
+
+
+}
